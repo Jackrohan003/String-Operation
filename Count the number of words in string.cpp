26ea87af1d1ba74b53c3851252aba4a3c6fe1bd5
Rohan Prajapati <58733914+Jackrohan003@users.noreply.github.com>
2019-12-11 09:45:20 +0530

    Create Count the number of words in string.cpp

---
+++ Count the number of words in string.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+//#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char s[100];
+    int count=0;
+    
+    cout<<"Enter the string ";
+    cin.getline(s,100);
+ 
+    char *p =s;
+    bool flag=false;
+    while(*p!='\0')
+    {
+        if(*p==' ' && *(p+1)!=' '&& *(p+1)!='\0') //*(p+1) ignore the multiple white space and null character.
+        {
+            count++;        
+        }
+    p++;
+    }
+
+    cout<<"number of words "<<count+1;
+
+}
