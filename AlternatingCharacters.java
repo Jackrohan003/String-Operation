3f4b33c7b8432419a5c67a792da5525dea91de55
Rohan Prajapati <58733914+Jackrohan003@users.noreply.github.com>
2020-04-28 10:26:35 +0530

    HackerRank Solution Of Alternating Characters

---
+++ AlternatingCharacters.java
@@ -0,0 +1,59 @@
+/*  
+  HackerRank Solution OF Alternating Characters 
+  https://www.hackerrank.com/challenges/alternating-characters/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the alternatingCharacters function below.
+    static int alternatingCharacters(String s) {
+
+        int cnt,j;
+        cnt = j =0;
+        for(int i=1;i<s.length();i++)
+        {
+            if(s.charAt(i)==s.charAt(j))
+            {
+                cnt++;j++;
+            }
+            else
+            {
+                j++;
+            }
+        }
+        
+
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = scanner.nextLine();
+
+            int result = alternatingCharacters(s);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
